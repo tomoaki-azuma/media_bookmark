a3111d1e9b1846354e5406428504c928e8ecdfe0
tomoaki-azuma <azumau@gmail.com>
2020-08-22 23:27:27 +0900

    modify qrcode generator

--- resources/views/mbm/index.blade.php
+++ resources/views/mbm/index.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 
+
+@php
+    $shared_url = url('').'/mbm/'; 
+@endphp
+
 @section('ogp-matadata')
 <meta property="og:title" content="{{ $bookmark->title }}">
 <meta property="og:description" content="{{ $bookmark->comment }}">
-<meta property="og:image" content="https://api.qrserver.com/v1/create-qr-code/?data={{ $bookmark->share_token }}&amp;size=200x200">
+<meta property="og:image" content="https://api.qrserver.com/v1/create-qr-code/?data={{ $shared_url.'/'.$bookmark->share_token }}&amp;size=200x200">
 @endsection
 
 @section('content')
